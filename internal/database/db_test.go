35f64a99a47c301181962e98f9ee3fb9e5f50ab5
Andy Dayton <amdayton@gmail.com>
2019-10-21 12:08:33 -0500

    Stub out testing for processing parse data for db

--- internal/database/db_test.go
+++ internal/database/db_test.go
@@ -1,12 +1,15 @@
 package database
 
 import (
+	"github.com/andyinabox/go-klippings-api/pkg/parser"
 	"os"
 	"testing"
 )
 
+const testDb = "../../test/tmp/test.db"
+const testDataFile = "../../test/data/my_clippings.txt"
+
 func TestOpen(t *testing.T) {
-	testDb := "../../test/tmp/test.db"
 	db, err := Open(testDb)
 	if err != nil {
 		t.Fatalf("Error opening db: %v", err)
@@ -18,5 +21,28 @@ func TestOpen(t *testing.T) {
 	err = os.Remove(testDb)
 	if err != nil {
 		t.Logf("Error remove test db: %v", err)
+	}
+}
+
+func TestProcessParseData(t *testing.T) {
+	f, err := os.Open(testDataFile)
+	if err != nil {
+		t.Fatalf("Error opening clippings file: %v", err)
+	}
+	defer f.Close()
+
+	data, err := parser.Parse(f)
+	if err != nil {
+		t.Fatalf("Error parsing data: %v", err)
+	}
+
+	db, err := Open(testDb)
+	if err != nil {
+		t.Fatalf("Error opening db: %v", err)
+	}
+
+	err = db.ProcessParseData(&data)
+	if err != nil {
+		t.Fatalf("Error processing parsed data: %v", err)
 	}
 }
